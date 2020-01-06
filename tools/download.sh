9f6fe5bcda169c268eb1c92ef00df9f61d540081
Linjie Li <linjieli222@gmail.com>
2020-01-06 13:56:25 -0800

    fix broken urls

--- tools/download.sh
+++ tools/download.sh
@@ -22,22 +22,24 @@ rm data/v2_Questions_Test_mscoco.zip
 
 # VQA Annotations
 wget -P data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip
+wget -P data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
 unzip data/v2_Annotations_Train_mscoco.zip -d data/Answers
 rm data/v2_Annotations_Train_mscoco.zip
-
-wget -P data https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
 unzip data/v2_Annotations_Val_mscoco.zip -d data/Answers
 rm data/v2_Annotations_Val_mscoco.zip
 
 # VQA cp-v2 Questions
+mkdir data/cp_v2_questions
 wget -P data/cp_v2_questions https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_train_questions.json 
 wget -P data/cp_v2_questions https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_test_questions.json 
 
 # VQA cp-v2 Annotations
+mkdir data/cp_v2_annotations
 wget -P data/cp_v2_annotations https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_train_annotations.json 
 wget -P data/cp_v2_annotations https://computing.ece.vt.edu/~aish/vqacp/vqacp_v2_train_annotations.json
 
 # Visual Genome Annotations
+mkdir data/visualGenome
 wget -P data/visualGenome https://convaisharables.blob.core.windows.net/vqa-regat/data/visualGenome/image_data.json
 wget -P data/visualGenome https://convaisharables.blob.core.windows.net/vqa-regat/data/visualGenome/question_answers.json
 
@@ -49,12 +51,14 @@ rm data/glove.zip
 # Image Features
 # adaptive
 # WARNING: This may take a while
+mkdir data/Bottom-up-features-adaptive
 wget -P data/Bottom-up-features-adaptive https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-adaptive/train.hdf5
 wget -P data/Bottom-up-features-adaptive https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-adaptive/val.hdf5
 wget -P data/Bottom-up-features-adaptive https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-adaptive/test2015.hdf5
 
 # fixed
 # WARNING: This may take a while
+mkdir data/Bottom-up-features-fixed
 wget -P data/Bottom-up-features-fixed https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-fixed/train36.hdf5
 wget -P data/Bottom-up-features-fixed https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-fixed/val36.hdf5
 wget -P data/Bottom-up-features-fixed https://convaisharables.blob.core.windows.net/vqa-regat/data/Bottom-up-features-fixed/test2015_36.hdf5
